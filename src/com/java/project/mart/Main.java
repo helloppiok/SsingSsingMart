87391b8baaa679e8f3620100903b245ff8edb5ed
helloppiok <pullip95@gmail.com>
2022-02-10 19:47:54 +0900

    [ADD] all code

---
+++ src/com/java/project/mart/Main.java
@@ -0,0 +1,52 @@
+package com.java.project.mart;
+
+public class Main { //테스트용
+	
+	public static void main(String[] args) throws Exception {
+		
+		ManagerMainFrame mmf = new ManagerMainFrame(); //관리자 초기화면
+		
+		//UserInfoList u = new UserInfoList(); //회원정보
+		
+		//ProductManagement pr = new ProductManagement(); //상품관리
+		
+		//SellManagement s = new SellManagement(); //매출관리
+		
+		//InventoryManagement i = new InventoryManagement(); //재고관리
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/java/project/mart/Path.java
@@ -0,0 +1,21 @@
+package com.java.project.mart;
+
+public class Path {
+	
+	//public static final String PRODUCTDIR = "/Users/hamgyusang/class/java/JavaTest/mart/Product.txt";
+	public static final String PRODUCTDIR = "D:\\class\\java\\JavaTest\\src\\com\\java\\project\\mart\\Product.txt";
+	
+	
+	//public static final String MEMBERDIR = "/Users/hamgyusang/class/java/JavaTest/mart/Member.txt";
+	public static final String MEMBERDIR = "D:\\class\\java\\JavaTest\\src\\com\\java\\project\\mart\\Member.txt";
+	
+	//public static final String PAYMENTLISTDIR = "/Users/hamgyusang/class/java/JavaTest/mart/PaymentList.txt";
+	public static final String PAYMENTLISTDIR = "D:\\class\\java\\JavaTest\\src\\com\\java\\project\\mart\\PaymentList.txt";
+	
+	//public static final String PRODIR = "/Users/hamgyusang/class/java/JavaTest/mart/pro.txt";
+	public static final String PRODIR = "D:\\class\\java\\JavaTest\\src\\com\\java\\project\\mart\\pro.txt";
+	
+	//public static final String PAYDIR = "/Users/hamgyusang/class/java/JavaTest/mart/pay.txt";
+	public static final String PAYDIR = "D:\\class\\java\\JavaTest\\src\\com\\java\\project\\mart\\pay.txt";
+
+}
